6a7105e6affbd9eb87e9a3c198ecf367a60b8334
Shivang Jain <129207095+shivangjain03@users.noreply.github.com>
2024-12-26 16:42:35 -0800

    Add files via upload

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,12 +1,25 @@
 import cv2
 import face_recognition
+#import face_recognition
 import pickle
 import os
 
-folderModePath = "Resources/Modes"
-modePath = os.listdir(folderModePath)
-imgModeList = []
-for imgPath in modePath:
-    img = cv2.imread(os.path.join(folderModePath, imgPath))
-    imgModeList.append(img)
+folderPath = "Images"
+Path = os.listdir(folderPath)
 
+imgList = []
+studentId=[]
+for imgPath in Path:
+    img = cv2.imread(os.path.join(folderPath, imgPath))
+    imgList.append(img)
+    studentId.append(os.path.splitext(imgPath)[0])
+print(studentId)
+
+
+def FindEncodings(imgList):
+    encodeList = []
+    for img in imgList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
